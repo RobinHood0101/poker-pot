05ce4ef9e48af5234134d9f9d2ba994db165def0
RobinHood0101 <robinkammer@proton.me>
2026-08-26 09:43:00 +0200

    add endGame business logic

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Enums\GameStatus;
+use App\Enums\TransactionType;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -20,6 +22,44 @@ class Game extends Model
         'status',
         'location',
     ];
+
+    public function endGame($cashAccount): void
+    {
+        if ($this->status !== GameStatus::ACTIVE->value) {
+            return;
+        }
+
+        $cashAccount = CashAccount::findOrFail($cashAccount);
+        $results = $this->results;
+        $amounts = [];
+        $contributionRuleItems = $this->contributionRule->items;
+        foreach ($results as $result) {
+            foreach ($contributionRuleItems as $contributionRuleItem) {
+                // get amount of money
+                if ($result->position === $contributionRuleItem->position) {
+                    $amounts[$result->user_id] = $contributionRuleItem->amount;
+                    break;
+                }
+
+                $amounts[$result->user_id] = $contributionRuleItems->last()->amount;
+            }
+        }
+
+        foreach ($amounts as $userId => $amount) {
+            CashTransaction::create([
+                'cash_account_id' => $cashAccount->id,
+                'user_id' => $userId,
+                'game_id' => $this->id,
+                'amount' => $amount,
+                'description' => 'Game '.$this->title.' end',
+                'type' => TransactionType::ACTIVE,
+            ]);
+        }
+
+        $this->played_at = now();
+        $this->status = GameStatus::INACTIVE->value;
+        $this->save();
+    }
 
     public function season(): BelongsTo
     {
